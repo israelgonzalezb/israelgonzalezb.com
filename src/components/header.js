95077a448c2c24aaf29a96ed5a975eac702c1f9d
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-05-10 02:40:44 -0700

    style: change colors and header links

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,30 @@
 import { Link } from "gatsby"
+import { Twitter, GitHub } from "react-feather"
 import PropTypes from "prop-types"
 import React from "react"
+
+let color1 = "#060101"
+let color2 = "#F22D40"
+let color3 = "#300608"
+let color4 = "#5A151B"
+let color5 = "#8D222D"
+let color6 = "#C13443"
+
+const linkStyle = {
+  textDecoration: `none`,
+  color: color2,
+}
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#000000`,
+      background: color1,
       marginBottom: `1.45rem`,
+      textDecoration: `none`,
+      display: `block`,
     }}
   >
+    {/*<h1 style={{ margin: 0 }}>
     <div
       style={{
         margin: `0 auto`,
@@ -16,19 +32,41 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             textDecoration: `none`, 
-            webkitTextFillColor: "#000000", /* Will override color (regardless of order) */
+            webkitTextFillColor: "#000000",
             webkitTextStrokeWidth: "1px",
             webkitTextStrokeColor: "#f92b40", 
           }}
         >
           {siteTitle}
         </Link>
-      </h1>
+        </h1>
+        </div>*/}
+    <div>
+      <Link to="/" style={{ ...linkStyle, textAlign: "center" }}>
+        <h1>Israel<span style={{ color: color5 }}>Gonzalez</span></h1>
+      </Link>
+    </div>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <span style={{ display: "flex", justifyContent: "space-between" }}>
+        <a href="https://github.com/israelgonzalezb" style={{...linkStyle, padding: "0 10px"}}>
+          <GitHub style={{verticalAlign: "bottom"}}/>
+        </a>
+        <a href="https://twitter.com/izzyz" style={{...linkStyle, padding: "0 10px"}}>
+          <Twitter style={{verticalAlign: "bottom"}}/>
+        </a>
+      </span>
+      <span style={{display: "flex", justifyContent: "space-between"}}>
+        <Link to="/" style={{...linkStyle, padding: "0 10px"}}>
+          About
+        </Link>
+        <Link to="/" style={{...linkStyle, padding: "0 10px"}}>
+          Projects
+        </Link>
+      </span>
     </div>
   </header>
 )
