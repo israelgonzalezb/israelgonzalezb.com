3acde8fdc8b4fdc23a97f3bf9ae43fb095baa6a9
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-05-12 02:58:45 -0700

    style: apply further styles to projects list component

--- src/components/project.js
+++ src/components/project.js
@@ -4,12 +4,10 @@ import Image from "./image"
 const Project = ({ thumb, title, path, source, description }) => {
   return (
     <li className="projectItem" style={{display: "table-row"}}>
-      <div className="projectHeader">
-        <div className="projectThumb" style={{ width: "72px", height: "72px" }}>
-          {thumb}
-        </div>
+      <div className="projectHeader" style={{display: "table-cell"}}>
+      <img className="projectThumb" style={{ width: "72px", height: "72px", borderRadius: "10px" }} src={thumb} />
       </div>
-      <div className="projectDetail">
+          <div className="projectDetail" style={{display: "table-cell", padding: "25px 0 39px"}}>
         <div className="projectTitle">
           <h3>{title}</h3>
         </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,20 @@
 export default [
   {
-    thumb: "catNamer.png",
+    thumb: "https://picsum.photos/72/72",
     title: "CAT NAMER",
     path: "https://google.com",
     source: "https://github.com/israelgonzalezb",
     description: "Name your cat.",
   },
   {
-    thumb: "boxCounter.png",
+    thumb: "https://picsum.photos/100/100",
     title: "Box Counter",
     path: "https://google.com",
     source: "https://github.com/israelgonzalezb",
     description: "Never manually count boxes again",
   },
   {
-    thumb: "sodaFlavorList.png",
+    thumb: "https://picsum.photos/80/80",
     title: "Soda Flavors List",
     path: "https://google.com",
     source: "https://github.com/israelgonzalezb",
